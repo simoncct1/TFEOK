6098f7279742edc32b48fe712cc4218fb0e9295b
Simon Couclet <simoncouclet@gmail.com>
2022-05-05 05:17:13 +0200

    df

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,13 +2,12 @@ import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../components/Home.vue'
 import About from '../components/About.vue'
-import Page2 from '../components/page2.vue'
+
 import App from '../components/App.vue'
 const router = createRouter( {
     history : createWebHistory(),
     routes: [
         {path: '/', name:'home' ,component: Home},
-        {path: '/page2', name:'page2' ,component: Page2},
         {path: '/about', name:'about' ,component: About}
     ]
 
